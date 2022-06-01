1f3d03cb5d69b94d36ba839e7491b84fbf2924a3
Willie <soo.willie@yahoo.co.id>
2022-06-01 18:51:54 +0700

    Platelet behaviour done

---
+++ Assets/Scripts/Platelet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platelet : MonoBehaviour
+{
+    [SerializeField] private Transform woundPosition;
+    [SerializeField] private float speed = 3f;
+
+    private bool isClosingWound = false;
+
+    private void Start()
+    {
+        woundPosition = GameObject.Find("Wound").transform;
+    }
+
+    private void Update()
+    {
+        if (!isClosingWound)
+        {        
+            transform.position = Vector2.MoveTowards(transform.position, woundPosition.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.name == "Wound")
+        {
+            //initialize close wound
+            transform.position = this.transform.position;
+            Debug.Log("Closing wound");
+        }
+    }
+}
