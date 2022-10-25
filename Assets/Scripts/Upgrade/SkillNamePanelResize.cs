9d4abd8695ba52f333ebcebe2918360a92b0b5aa
AirCaptainx99 <80711605+AirCaptainx99@users.noreply.github.com>
2022-10-25 14:33:13 +0700

    Added Skill Name Panel

---
+++ Assets/Scripts/Upgrade/SkillNamePanelResize.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SkillNamePanelResize : MonoBehaviour
+{
+    [SerializeField] private GameObject skillName;
+    private RectTransform panelTransform;
+
+    void Start()
+    {
+        panelTransform = gameObject.GetComponent<RectTransform>();
+        RectTransform textRectTransform = skillName.GetComponent<RectTransform>();
+        TextMeshProUGUI tmp = skillName.GetComponent<TextMeshProUGUI>();
+        tmp.ForceMeshUpdate();
+
+        Vector3 newPanelSize = tmp.textBounds.size;
+        newPanelSize.x += Mathf.Abs(textRectTransform.localPosition.x * 2);
+        print(newPanelSize.y);
+        newPanelSize.y += 20f;
+        panelTransform.sizeDelta = newPanelSize;
+
+    }
+}
