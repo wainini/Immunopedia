fea72335f6688e37d08ca25c9ebd9fa650026a8f
AirCaptainx99 <80711605+AirCaptainx99@users.noreply.github.com>
2022-10-28 12:37:10 +0700

    Fixed Popup UI Resizing

--- Assets/Scripts/Upgrade/ResizePopUpUI.cs
+++ Assets/Scripts/Upgrade/ResizePopUpUI.cs
@@ -6,9 +6,21 @@ public class ResizePopUpUI : MonoBehaviour
 {
     [SerializeField] private GameObject skillNamePanel;
     [SerializeField] private GameObject skillDescriptionPanel;
+
+    private SkillDescriptionPanelResize goDesc;
+    private SkillNamePanelResize goName;
+
+    private void Start()
+    {
+        goDesc = skillDescriptionPanel.GetComponent<SkillDescriptionPanelResize>();
+        goName = skillNamePanel.GetComponent<SkillNamePanelResize>();
+    }
     public void ResizeUI()
     {
-        skillDescriptionPanel.GetComponent<SkillDescriptionPanelResize>().ResizePanel();
-        skillNamePanel.GetComponent<SkillNamePanelResize>().ResizePanel();
+        if (goDesc != null && goName != null)
+        {
+           goDesc.ResizePanel();
+           goName.ResizePanel();
+        }
     }
 }
